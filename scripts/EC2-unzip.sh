8364c6ca02dd420a25d3715d2db84b58ff6ba7a4
Nandana-Chigaterappa-HemanthKumar <Nandana-Chigaterappa-HemanthKumar>
2024-12-04 19:49:47 +0000

    Structure Update

--- scripts/EC2-unzip.sh
+++ scripts/EC2-unzip.sh
@@ -2,15 +2,15 @@
 
 # This script creates an EC2 instance using AWS CLI
 
-# Set variables for instance creation
-AMI_ID="ami-12345678"  # Replace with your desired AMI ID
-INSTANCE_TYPE="c5.4xlarge"  # Instance type (adjusted based on the screenshot)
-KEY_NAME="unzip.pem"  # Replace with the name of your key pair
-SECURITY_GROUP="sg-08a27bf3bde82465a"  # Updated security group ID based on the screenshot
-SUBNET_ID="subnet-07774e143495ee1ac"  # Updated subnet ID based on the screenshot
-TAG_NAME="unzip"  # Instance tag name updated based on the screenshot
+# Setting variables for instance creation
+AMI_ID="ami-12345678"  
+INSTANCE_TYPE="c5.4xlarge"  
+KEY_NAME="unzip.pem"  
+SECURITY_GROUP="sg-08a27bf3bde82465a"  
+SUBNET_ID="subnet-07774e143495ee1ac"  
+TAG_NAME="unzip" 
 
-# Create the EC2 instance
+# Creating the EC2 instance
 echo "Creating EC2 instance..."
 INSTANCE_ID=$(aws ec2 run-instances \
   --image-id $AMI_ID \
@@ -22,7 +22,7 @@ INSTANCE_ID=$(aws ec2 run-instances \
   --query 'Instances[0].InstanceId' \
   --output text)
 
-# Wait for the instance to reach the running state
+# Waiting for the instance to reach the running state
 echo "Waiting for instance to be in running state..."
 aws ec2 wait instance-running --instance-ids $INSTANCE_ID
 
